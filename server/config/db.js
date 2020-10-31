a6fd97bfb778049b33ce5cb511fbbcb6b2406ac8
jkuzhila <60558703+jkuzhila@users.noreply.github.com>
2020-10-30 20:13:03 -0400

    Add files via upload

---
+++ server/config/db.js
@@ -0,0 +1,5 @@
+/* File name : db.js  Author's name : James Kuzhilaparambil Student ID: 301119040 Web App name: COMP229-W2020-MidTerm-3001119040 */
+module.exports = {
+  //local MongoDB deployment ->
+  "URI": "https://cloud.mongodb.com/v2/5f9c3610eea7cc33e63e2f5d#metrics/replicaSet/5f9c36bf78d93d6554247084/explorer/COMP229-F2020-Midterm-3001119040/test/find"
+};
